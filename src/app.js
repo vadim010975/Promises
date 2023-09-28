da793fb09557a06d49194a7189d2cdb683486a50
vadim010975 <vadim010975@mail.ru>
2023-09-28 21:31:32 +0300

    add fixed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import GameSavingLoader from './GameSavingLoader';
 
-export default GameSavingLoader.load().then((saving) => {
+GameSavingLoader.load().then((saving) => {
   console.log(saving);
 }, (error) => {
   console.error(error);
